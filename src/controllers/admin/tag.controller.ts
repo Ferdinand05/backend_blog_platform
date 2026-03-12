b3ff3a746cd54c23142fef6c99842f8b4781b064
Ferdinand05 <fkoryanto@gmail.com>
2026-03-12 18:00:16 +0700

    add crud operations for tag modul

--- src/controllers/admin/tag.controller.ts
+++ src/controllers/admin/tag.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import db from "../../models";
 import { tagSchema } from "../../validators/tag.validator";
-import z from "zod";
+import z, { number } from "zod";
 
 export async function getAllTag(req: Request, res: Response) {
   const tags = await db.Tag.findAll();
@@ -30,6 +30,55 @@ export async function createTag(req: Request, res: Response) {
   }
 }
 
-export async function updateTag(req: Request, res: Response) {}
+export async function updateTag(req: Request, res: Response) {
+  const { id } = req.params;
 
-export async function deleteTag(req: Request, res: Response) {}
+  const parsed = tagSchema.safeParse(req.body);
+
+  if (!parsed.success) {
+    return res.status(400).json({
+      message: "Validation Error",
+      error: z.treeifyError(parsed.error),
+    });
+  }
+
+  try {
+    const { name } = parsed.data;
+
+    const tag = (await db.Tag.findByPk(Number(id))) as any;
+
+    if (!tag) {
+      return res.status(404).json({
+        message: "Tag not found",
+      });
+    }
+
+    tag.name = name;
+    await tag.save();
+
+    return res.status(201).json({
+      message: "Slug Updated successfully.",
+      tag,
+    });
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+}
+
+export async function deleteTag(req: Request, res: Response) {
+  const { id } = req.params;
+
+  const tag = await db.Tag.destroy({
+    where: { id },
+  });
+
+  if (!tag) {
+    return res.status(404).json({
+      message: "Tag not found",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Tag deleted successfully.",
+  });
+}

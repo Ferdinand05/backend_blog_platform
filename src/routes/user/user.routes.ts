ca25c1cd2d7de40c5f7b0c4aaea122ddaa5fa3d0
Ferdinand05 <fkoryanto@gmail.com>
2026-03-17 21:28:36 +0700

    feat(user) : add updateProfile and ChangePassword for auth user and fix getMe data

--- src/routes/user/user.routes.ts
+++ src/routes/user/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { getMe, updateProfile } from "../../controllers/user/user.controller";
+import {
+  changePassword,
+  getMe,
+  updateProfile,
+} from "../../controllers/user/user.controller";
+import { upload } from "../../middleware/upload.middleware";
 
 const router = Router();
 
-router.route("/").get(getMe).put(updateProfile);
+router.route("/").get(getMe).put(upload.single("avatar"), updateProfile);
+router.put("/password", changePassword);
 
 export default router;
--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -11,3 +11,13 @@ export const userUpdateSchema = z.object({
   username: z.string().min(3).max(100),
   role_id: z.int(),
 });
+
+export const updateProfileSchema = z.object({
+  username: z.string().min(3).max(100).optional(),
+  bio: z.string().min(3).optional(),
+});
+
+export const changePasswordSchema = z.object({
+  current_password: z.string().min(7),
+  new_password: z.string().min(7),
+});
